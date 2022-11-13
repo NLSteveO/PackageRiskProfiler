ad7ddff4181720d1331f1456cc27f36914216cb2
NLSteveO <steveokeefe88@gmail.com>
2022-11-12 22:47:42 -0330

    Add label input and button to get package name
    Then call the npms api for the package to get the packages score. Add a hook for storing the package name and a hook for storing the score from the api call.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
+const getNpmsScores = (pkg, setScore) => {
+    fetch('http://api.npms.io/v2/package/' + pkg)
+    .then(response => response.json().then(blah => setScore({[pkg]: blah.score})));
+};
+
 function App() {
   const [count, setCount] = useState(0);
+  const [pkg, setPackage] = useState('');
+  const [score, setScore] = useState({});
+  const updatePackage = evt => setPackage(evt.target.value);
+  const submit = () => getNpmsScores(pkg, setScore);
 
   return (
     <div className="App">
@@ -23,6 +32,11 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div className="card">
+        <label>Package Name: </label>
+        <input onChange={updatePackage} />
+        <button onClick={submit} >Check</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
